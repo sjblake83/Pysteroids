9ca44d57251d060e2a921a9eca99f390df4fd3f6
sjblake83 <sjblake83@gmail.com>
2025-10-01 21:06:13 -0400

    Implement asteroid field

---
+++ asteroid.py
@@ -0,0 +1,18 @@
+import pygame
+from circleshape import CircleShape
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+    
+    def draw(self, screen):
+        return pygame.draw.circle(
+            screen, 
+            "red", 
+            self.position, 
+            self.radius, 
+            2)
+    
+    def update(self, dt):
+        # forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += self.velocity * dt
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting Asteroids!")
@@ -14,12 +16,16 @@ def main():
     
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     
     Player.containers = {updatable, drawable}
+    Asteroid.containers = {updatable, drawable, asteroids}
+    AsteroidField.containers = {updatable}
     
     screen_midway_x = SCREEN_WIDTH / 2
     screen_midway_y = SCREEN_HEIGHT / 2
     player = Player(screen_midway_x, screen_midway_y)
+    asteroid_field = AsteroidField()
 
     while(True):
         for event in pygame.event.get():
