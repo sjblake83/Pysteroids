91046d32d29b5342aebe9f4aee6753be983ce226
sjblake83 <sjblake83@gmail.com>
2025-10-01 20:50:15 -0400

    Enhance movement, add groups/containers

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = {updatable, drawable}
+    
     screen_midway_x = SCREEN_WIDTH / 2
     screen_midway_y = SCREEN_HEIGHT / 2
     player = Player(screen_midway_x, screen_midway_y)
@@ -21,8 +26,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for object in drawable:
+            object.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
         dt = (clock.tick(60.0) / 1000)
 
